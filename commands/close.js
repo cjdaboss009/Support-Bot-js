5e159d161a26197f99d4f952a9c825b3016aec90
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-12-06 04:29:17 +0000

    Close command completed.

---
+++ commands/close.js
@@ -0,0 +1,13 @@
+
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const client = new Discord.Client();
+message.reply("Ticket closing.");
+{(;
+guild.channel.delete();
+console.log('The channel was deleted');
+{(;
+module.exports.help = {
+    name: "close",
+    aliases: "Close"
+    category: "Commands"
