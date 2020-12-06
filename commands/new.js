d72d0010f57487c67a9914e7e95225361b8edc00
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-12-06 04:20:47 +0000

    Not done.

---
+++ commands/new.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const client = new Discord.Client();
+
+guild.channel.create("");
+SEND_MESSAGES: TRUE
+ADD_REACTION: FALSE
+
+message.reply("Your ticket was created ${Ticket} ");
