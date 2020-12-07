c918a3eec421e2afe21f571d1e37946b4a8c1e63
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-12-07 23:56:09 +0000

    done

--- commands/setup.js
+++ commands/setup.js
@@ -5,5 +5,12 @@ message.channel.send("Setting up logs.");
 guild.channel.find("logs");
 else { message.channel.send("Couldn't find a log channel.");
       if guild.channel.find("logs");
-      
-      
+      console.log("");
+      {(;
+        else { message.reply("Please retry setting up");   
+      message.reply("All done!");
+module.exports.help = {
+    name: "setup",
+    aliases: "s,Setup",
+    category: "commands"
+  }
