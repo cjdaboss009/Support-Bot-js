3605727fd334b4e6a04a5d0fe94327a869add2ef
cjdaboss009 <67497735+cjdaboss009@users.noreply.github.com>
2020-12-07 23:33:51 +0000

    Update and rename new.js to setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -1,14 +1,9 @@
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
-const Embed = new Discord.MessageEmbed()
-
-guild.channel.create("ticket-${random-numbers}");
-SEND_MESSAGES: TRUE
-ADD_REACTION: FALSE
 guild.channel.find("logs");
 else { message.channel.send("Couldn't find a log channel.");
       if guild.channel.find("logs");
-      then
+      message.reply 
 message.reply("Your ticket was created ${Ticket} ");
 message.react("✅");
